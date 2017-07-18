ef19eb58b6cce6a6f203dc5cae99a12c3cb8d6aa
Frank Blaauw <frank.blaauw@gmail.com>
2017-07-18 13:56:21 +0200

    Update

---
+++ arxiv-over-time/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+./get_data.rb
+./plot_data.r
+pdfcrop ml-google-trends.pdf ml-google-trends.pdf
+mv ml-google-trends.pdf ../../thesis/fblaauw_thesis/images/chapter2/ml-google-trends.pdf
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
-./get_data.rb
-./plot_data.r
-mv ml-google-trends.pdf ../thesis/fblaauw_thesis/images/chapter2/ml-google-trends.pdf
+for folder in */; do
+  echo "Executing $folder"
+  cd $folder
+  ./run.sh
+  cd -
+done
